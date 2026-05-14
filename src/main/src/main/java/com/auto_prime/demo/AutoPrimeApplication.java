b99be9b976b17c04ee10574653bb2214e7f3add8
PedroHLJR <pedro.hljr2001@gmail.com>
2026-05-13 23:27:03 -0300

    Initialize AutoPrimeApplication with Spring Boot

--- src/main/src/main/java/com/auto_prime/demo/AutoPrimeApplication.java
+++ src/main/src/main/java/com/auto_prime/demo/AutoPrimeApplication.java
@@ -1,1 +1,13 @@
+package com.auto_prime.demo;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoPrimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoPrimeApplication.class, args);
+	}
+
+}
